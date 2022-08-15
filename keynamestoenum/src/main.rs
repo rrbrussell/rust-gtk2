881232d9bf6765ca6c5461ad51eb4b16ce4bc94e
Robert R. Russell <robert@rrbrussell.com>
2022-08-14 19:18:08 -0500

    Keynamestoenum program has been built.
    It converts the keytable.txt file into a rust enum.

--- keynamestoenum/src/main.rs
+++ keynamestoenum/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+/*
+enum Keys {
+    H = 0x000054,
+    t = 0x000074,
+} */
+
+use std::env;
+use std::io;
+use std::io::BufRead;
+use std::io::BufWriter;
+use std::io::BufReader;
+use std::io::Write;
+use std::fs::File;
+
+static START_OF_KEYS_ENUM: &str = "pub enum Keys {\n";
+static END_OF_KEYS_ENUM: &str = "}\n";
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 || args.len() == 1 {
+        println!("Usage: keynamestoenum <input_file>");
+        return Ok(());
+    }
+
+    let in_file = &args[1];
+    let mut output = BufWriter::new(io::stdout());
+    
+    let in_lines = BufReader::new(File::open(in_file)?).lines();
+    _ = write!(output, "{}", START_OF_KEYS_ENUM);
+    for line in in_lines {
+        match line {
+            Err(_e) => {},
+            Ok(read) => {
+                let parts = Vec::from_iter(read.split_whitespace());
+                _ = writeln!(output, "{}: {},", parts[1], parts[0]);
+            }
+        }
+    }
+    _ = write!(output, "{}", END_OF_KEYS_ENUM);
+    return Ok(());
 }
