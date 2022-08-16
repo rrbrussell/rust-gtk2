ceb11ed979f5162347e9d5b7580178576d8524e3
Robert R. Russell <robert@rrbrussell.com>
2022-08-15 23:51:28 -0500

    keynamestoenum will now create a valid vector

--- keynamestoenum/src/main.rs
+++ keynamestoenum/src/main.rs
@@ -4,15 +4,20 @@ enum Keys {
     t = 0x000074,
 } */
 
+#[allow(dead_code)]
 use std::env;
 use std::io;
 use std::io::BufRead;
-use std::io::BufWriter;
-use std::io::BufReader;
-use std::io::Write;
+//use std::io::BufWriter;
+use std::fmt::Debug;
 use std::fs::File;
+use std::io::BufReader;
+use std::vec::Vec;
+//use std::str::FromStr;
 
+#[allow(dead_code)]
 static START_OF_KEYS_ENUM: &str = "pub enum Keys {\n";
+#[allow(dead_code)]
 static END_OF_KEYS_ENUM: &str = "}\n";
 
 fn main() -> Result<(), io::Error> {
@@ -22,20 +27,48 @@ fn main() -> Result<(), io::Error> {
         return Ok(());
     }
 
+    let mut keytable: Vec<KeyTableEntry> = Vec::with_capacity(2000);
+
     let in_file = &args[1];
-    let mut output = BufWriter::new(io::stdout());
-    
+
     let in_lines = BufReader::new(File::open(in_file)?).lines();
-    _ = write!(output, "{}", START_OF_KEYS_ENUM);
     for line in in_lines {
         match line {
-            Err(_e) => {},
+            Err(_e) => {}
             Ok(read) => {
-                let parts = Vec::from_iter(read.split_whitespace());
-                _ = writeln!(output, "{}: {},", parts[1], parts[0]);
+                let parts: Vec<&str> = read.split_whitespace().collect();
+                //println!("{:?}",parts);
+                let mut think = String::from("KEY_");
+                think.push_str(parts[1]);
+                let without_prefix = parts[0].trim_start_matches("0x");
+                let temp = KeyTableEntry {
+                    identifier: think,
+                    name: String::from(parts[1]),
+                    keycode: i32::from_str_radix(without_prefix, 16).unwrap(),
+                };
+                keytable.push(temp.clone());
             }
         }
     }
-    _ = write!(output, "{}", END_OF_KEYS_ENUM);
+
+    print!("[");
+    let mut counter: usize = 0;
+    for entry in keytable.iter() {
+        print!(" {:?},", entry);
+        counter += 1;
+        if counter == 2 {
+            print!("\n");
+            counter = 0;
+        }
+    }
+    print!("]");
     return Ok(());
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+struct KeyTableEntry {
+    identifier: String,
+    name: String,
+    keycode: i32,
 }
